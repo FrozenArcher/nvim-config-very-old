acc4bf08077f2651ea9e09525ba50d9e2b32101f
Hongyi <61831273+FrozenArcher@users.noreply.github.com>
2022-06-02 20:04:01 +0800

    refactor config

--- lua/config.lua
+++
@@ -1,8 +0,0 @@
-require("tree-cfg")
-require("buffer-cfg")
-require("lualine-cfg")
-require("sitter-cfg")
-
-require("scrollbar").setup()
-
-vim.cmd("colorscheme tokyonight")
---
+++ lua/config/init.lua
@@ -0,0 +1,8 @@
+require("config.tree-cfg")
+require("config.buffer-cfg")
+require("config.lualine-cfg")
+require("config.sitter-cfg")
+
+require("scrollbar").setup()
+
+vim.cmd("colorscheme tokyonight")
