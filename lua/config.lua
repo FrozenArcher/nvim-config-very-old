e2d122c200e8393d4615ddc6263f1da96ae0b789
Hongyi <61831273+FrozenArcher@users.noreply.github.com>
2022-06-02 19:30:35 +0800

    scrollbar

--- lua/config.lua
+++ lua/config.lua
@@ -3,4 +3,6 @@ require("buffer-cfg")
 require("lualine-cfg")
 require("sitter-cfg")
 
+require("scrollbar").setup()
+
 vim.cmd("colorscheme tokyonight")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,8 +6,8 @@ return require("packer").startup(function(use)
 	use({ "kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons" })
 	use({ "akinsho/bufferline.nvim", tag = "v2.*", requires = "kyazdani42/nvim-web-devicons" })
 	use("folke/tokyonight.nvim")
-	--use {"NTBBloodbath/galaxyline.nvim", requires = { "kyazdani42/nvim-web-devicons", opt = true }}
 	use({ "nvim-lualine/lualine.nvim", requires = { "kyazdani42/nvim-web-devicons", opt = true } })
     use "ms-jpq/coq_nvim"
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+    use {"petertriho/nvim-scrollbar"}
 end)
